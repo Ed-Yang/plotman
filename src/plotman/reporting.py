62da5007867bbc3ad44936284fef0a205ab64d5c
Ed-Yang <edwardyangyang@hotmail.com>
2021-05-24 19:45:40 +0800

    k25 and #234

--- src/plotman/reporting.py
+++ src/plotman/reporting.py
@@ -4,8 +4,7 @@ import os
 import psutil
 import texttable as tt  # from somewhere?
 
-from plotman import archive, job, manager, plot_util
-
+from plotman import archive, configuration, job, manager, plot_util
 
 def abbr_path(path, putative_prefix):
     if putative_prefix and path.startswith(putative_prefix):
@@ -188,9 +187,10 @@ def arch_dir_report(archdir_freebytes, width, prefix=''):
 
 # TODO: remove this
 def dirs_report(jobs, dir_cfg, sched_cfg, width):
+    dst_dir = dir_cfg.get_dst_directories()
     reports = [
         tmp_dir_report(jobs, dir_cfg, sched_cfg, width),
-        dst_dir_report(jobs, dir_cfg.dst, width),
+        dst_dir_report(jobs, dst_dir, width),
     ]
     if dir_cfg.archive is not None:
         reports.extend([
